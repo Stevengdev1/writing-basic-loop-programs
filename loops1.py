69473f2c8842258d13921f4cff9a5a94c2469125
Steven <stevendg922@gmail.com>
2023-12-14 22:35:12 -0500

    Created basic loop programs.

---
+++ loops1.py
@@ -0,0 +1,38 @@
+# loops1.py project
+# 11-13-2023
+# by Steven D. Garcia
+
+# Exercise #1
+
+for a in range(1, 6):
+    print(a)
+print()
+
+for b in range(2, 12, 3):
+    print(b)
+print()
+
+for c in range(-10, 0, 2):
+    print(c)
+print()
+
+for d in range(1, 5):
+    print("****")
+print()
+
+# Exercise 2
+
+word = input("Enter a word: ")
+print("***")
+for letter in word:
+    print(letter)
+print("***")
+# # Another way this could be done is the following:
+for c in "CSCI 150":
+    print(c)
+
+# Exercise 3
+    
+for nums in range(1, 11):
+    print(nums)
+
